7d25720a768c43b6a2158e659ad393607e57f6b3
Matte2k <matteob2000@hotmail.it>
2024-06-10 19:55:22 +0200

    Onera validation refactor PART 5
    Added aeroloads plots for Onera validation

--- DUST-validation/oneraM6-wing/aeroloads_compare.m
+++ DUST-validation/oneraM6-wing/aeroloads_compare.m
@@ -4,6 +4,8 @@ addpath(genpath("./data"))
 currentPath = pwd;
 [~] = initGraphic;
 
+%% Data import
+
 % CFD loads data
 CFD.mach03 = outputCFD_mach03;
 CFD.mach08 = outputCFD_mach08;
@@ -13,10 +15,116 @@ DUST.mach03 = outputDUST_mach03;
 DUST.mach08 = outputDUST_mach08;
 
 % AVL loads data
-AVL.mach03 = outputAVL_mach03();
-AVL.mach08 = outputAVL_mach08();
+AVL.mach03 = outputAVL_mach03;
+AVL.mach08 = outputAVL_mach08;
 
-% AVL loads data
-XFLR5.mach03 = outputXFLR5_mach03();
-XFLR5.mach08 = outputXFLR5_mach08();
+% XFLR5 loads data
+XFLR5.mach03 = outputXFLR5_mach03;
+XFLR5.mach08 = outputXFLR5_mach08;
 
+
+%% Polars plot - mach 0.3000
+
+figure("Name",'lift vs alpha')
+title('wing L vs $\alpha$ at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.flow.aoaDeg ,   CFD.mach03.aero.Cl,  '-o');
+plot(DUST.mach03.flow.aoaDeg ,  DUST.mach03.aero.Cl, '-o');
+plot(AVL.mach03.flow.aoaDeg ,   AVL.mach03.aero.Cl,  '-o');
+plot(XFLR5.mach03.flow.aoaDeg , XFLR5.mach03.aero.Cl,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southeast')
+xlabel('$\alpha$');      ylabel('$C_L$');
+
+figure("Name",'drag vs alpha')
+title('wing D vs $\alpha$ at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.flow.aoaDeg ,   CFD.mach03.aero.Cd,  '-o');
+plot(DUST.mach03.flow.aoaDeg ,  DUST.mach03.aero.Cd, '-o');
+plot(AVL.mach03.flow.aoaDeg ,   AVL.mach03.aero.Cd,  '-o');
+plot(XFLR5.mach03.flow.aoaDeg , XFLR5.mach03.aero.Cd,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='northwest')
+xlabel('$\alpha$');      ylabel('$C_D$');
+
+figure("Name",'moment vs alpha')
+title('pitching moment vs $\alpha$ at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.flow.aoaDeg ,   CFD.mach03.aero.Cm,  '-o');
+plot(DUST.mach03.flow.aoaDeg ,  DUST.mach03.aero.Cm, '-o');
+plot(AVL.mach03.flow.aoaDeg ,   AVL.mach03.aero.Cm,  '-o');
+plot(XFLR5.mach03.flow.aoaDeg , XFLR5.mach03.aero.Cm,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southwest')
+xlabel('$\alpha$');      ylabel('$C_M$');
+
+figure("Name",'lift vs drag')
+title('wing polar at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.aero.Cd ,   CFD.mach03.aero.Cl,  '-o');
+plot(DUST.mach03.aero.Cd ,  DUST.mach03.aero.Cl, '-o');
+plot(AVL.mach03.aero.Cd ,   AVL.mach03.aero.Cl,  '-o');
+plot(XFLR5.mach03.aero.Cd , XFLR5.mach03.aero.Cl,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southeast')
+xlabel('$C_D$');      ylabel('$C_L$');
+
+
+%% Polars plot - mach 0.8395
+
+figure("Name",'lift vs alpha')
+title('wing L vs $\alpha$ at $$Ma=0.8395$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach08.flow.aoaDeg ,   CFD.mach08.aero.Cl,  '-o');
+plot(DUST.mach08.flow.aoaDeg ,  DUST.mach08.aero.Cl, '-o');
+plot(AVL.mach08.flow.aoaDeg ,   AVL.mach08.aero.Cl,  '-o');
+plot(XFLR5.mach08.flow.aoaDeg , XFLR5.mach08.aero.Cl,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southeast')
+xlabel('$\alpha$');      ylabel('$C_L$');
+
+figure("Name",'drag vs alpha')
+title('wing D vs $\alpha$ at $$Ma=0.8395$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach08.flow.aoaDeg ,   CFD.mach08.aero.Cd,  '-o');
+plot(DUST.mach08.flow.aoaDeg ,  DUST.mach08.aero.Cd, '-o');
+plot(AVL.mach08.flow.aoaDeg ,   AVL.mach08.aero.Cd,  '-o');
+plot(XFLR5.mach08.flow.aoaDeg , XFLR5.mach08.aero.Cd,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='northwest')
+xlabel('$\alpha$');      ylabel('$C_D$');
+
+figure("Name",'moment vs alpha')
+title('pitching moment vs $\alpha$ at $$Ma=0.8395$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach08.flow.aoaDeg ,   CFD.mach08.aero.Cm,  '-o');
+plot(DUST.mach08.flow.aoaDeg ,  DUST.mach08.aero.Cm, '-o');
+plot(AVL.mach08.flow.aoaDeg ,   AVL.mach08.aero.Cm,  '-o');
+plot(XFLR5.mach08.flow.aoaDeg , XFLR5.mach08.aero.Cm,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southwest')
+xlabel('$\alpha$');      ylabel('$C_M$');
+
+figure("Name",'lift vs drag')
+title('wing polar at $$Ma=0.8395$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach08.aero.Cd ,   CFD.mach08.aero.Cl,  '-o');
+plot(DUST.mach08.aero.Cd ,  DUST.mach08.aero.Cl, '-o');
+plot(AVL.mach08.aero.Cd ,   AVL.mach08.aero.Cl,  '-o');
+plot(XFLR5.mach08.aero.Cd , XFLR5.mach08.aero.Cl,'-o');
+legend('CFD','DUST','AVL','XFLR5',location='southeast')
+xlabel('$C_D$');      ylabel('$C_L$');
+
+
+%%  STRUCTURAL LOAD SUBSONIC X and Z
+
+figure("Name",'x-force vs alpha')
+title('wing Fx vs $\alpha$ at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.flow.aoaDeg ,   CFD.mach03.struct.Cfx,  '-o');
+plot(DUST.mach03.flow.aoaDeg ,  DUST.mach03.struct.Cfx, '-o');
+plot(AVL.mach03.flow.aoaDeg ,   AVL.mach03.struct.Cfx,  '-o');
+legend('CFD','DUST','AVL',location='southwest')
+xlabel('$\alpha$');      ylabel('$C_X$');
+
+figure("Name",'z-force vs alpha')
+title('wing Fz vs $\alpha$ at $$Ma=0.3000$$')
+hold on;    grid on;    axis padded;
+plot(CFD.mach03.flow.aoaDeg ,   CFD.mach03.struct.Cfz,  '-o');
+plot(DUST.mach03.flow.aoaDeg ,  DUST.mach03.struct.Cfz, '-o');
+plot(AVL.mach03.flow.aoaDeg ,   AVL.mach03.struct.Cfz,  '-o');
+legend('CFD','DUST','AVL',location='southwest')
+xlabel('$\alpha$');      ylabel('$C_Z$');
