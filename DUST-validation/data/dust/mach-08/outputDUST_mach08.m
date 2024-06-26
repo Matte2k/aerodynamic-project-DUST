6a19d8ba7f129373bc7de1cffdafee8eaaa106e3
Matte2k <matteob2000@hotmail.it>
2024-06-26 23:29:24 +0200

    fix DUST-validation
    In an older commit two key functions were eliminated. Old function recovered from older version of the repo.

---
+++ DUST-validation/data/dust/mach-08/outputDUST_mach08.m
@@ -0,0 +1,62 @@
+function [data] = outputDUST_mach08(dataPath)
+%OUTPUT DUST at MACH 0.8395
+%
+%   Syntax:
+%       [data] = outputDUST_mach08(dataPath)
+%
+%   Input:
+%       dataPath(*),  path: path of the file containing designData of the DUST run
+%
+%   Output:
+%       data, struct: contains the following fields:
+%                   - ref:    reference value used to compute loads
+%                   - flow:   free stream condition
+%                   - aero:   aerodynamic force and moment coefficient
+%                   - struct: structural force and moment coefficient
+%
+%   NOTE:   the reference and flow values can be edited while, all the
+%           data in this function has been reported automatic from:
+%               > ./data/cfd/mach-08/oneraM6_mach08.mat
+%           The current reference system directions are:
+%                   CURRENT REFERENCE: x-backward, y-right, z-up
+%
+%                               Matteo Baio, Politecnico di Milano, 06/2024
+%
+
+    if nargin < 1
+        dataPath = './data/dust/mach-08/oneraM6_mach08.mat';
+    end
+
+    % reference values
+    data.ref.S   = 0.758602;
+    data.ref.C   = 1;
+    data.ref.rho = 1.22498;
+    data.ref.P   = 99973.8;
+    data.ref.v   = 285.679;
+
+    % panel method data
+    data.mesh.chordElem = 20;
+    data.mesh.spanElem  = 50;
+ 
+    dustOutput = load(dataPath);
+    [aeroLoads]    = aeroLoads_DUST   (dustOutput.designData, data.ref.v , data.ref.rho, data.ref.S, data.ref.C, false);
+    [structLoads]  = structLoads_DUST (dustOutput.designData, data.ref.v , data.ref.rho, data.ref.S, data.ref.C, 'aoa', false);
+
+    % flow data
+    data.flow.mach   = 0.8395;
+    data.flow.aoaDeg = [ 0.000000,     3.060000,    6.120000];
+   
+    % structural loads
+    data.struct.Cfx  = structLoads.Cfx;
+    data.struct.Cfy  = structLoads.Cfy;
+    data.struct.Cfz  = structLoads.Cfz;
+    data.struct.Cmx  = structLoads.Cmx;
+    data.struct.Cmy  = structLoads.Cmy;
+    data.struct.Cmz  = structLoads.Cmz;
+
+    % aerodynamic loads
+    data.aero.Cl   = aeroLoads.Cl;
+    data.aero.Cd   = aeroLoads.Cd;
+    data.aero.Cm   = aeroLoads.Cm;
+
+end
